744f21c8f6e28e7137b1acf37c2c42ff956e8300
Stan92Islav <amaxis1900@gmail.com>
2024-05-15 16:49:13 +0300

    most common word

--- II_step13_chapter10.3 (most common word).py
+++ II_step13_chapter10.3 (most common word).py
@@ -3,8 +3,25 @@
 
 s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
 
+# Решение №1 Counter
 from collections import Counter
-a = dict(Counter(s.split()))
 
+result = list(sorted(Counter(s.split()).most_common()[:2], key=lambda x: x[0]))[0][0]
+print(result)
+
+# Решение №2 get
+
+result2 = {}
+for i in s.split():
+    result2[i] = result2.get(i, 0) + 1
+
+mx = max(result2.values())
+
+result3 = {}
+for key, value in result2.items():
+    if value == mx:
+        result3[key] = result3.get(key, value)
+
+print(min(result3))
 
 
